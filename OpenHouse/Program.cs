90c9262da981ffb07c21ca1ad6446f098ddc625a
Chip <chip.elmer@gmail.com>
2018-08-22 09:07:15 -0500

    Modified if statements to remove dependency on previous question when checking for each new question

--- OpenHouse/Program.cs
+++ OpenHouse/Program.cs
@@ -26,80 +26,71 @@ namespace OpenHouse
 
                 if (buyerResponse == "y")
                 {
-
                     Console.WriteLine("Dinningroom has Wainwritght walls with cheery wood borders enough space tp set 12 comfortably.");
                     Console.WriteLine("Kitchen has stainless steel appliances an gourmet island with granite countertops and authentic Panamian tile shipped from Central America.");
                     Console.WriteLine();
-                    Console.WriteLine("Would you like to tour the next see the basement?");
-
-                    buyerResponse = Console.ReadLine();
                     Console.ReadLine();
                 }
+
+                Console.WriteLine("Would you like to tour the next see the basement?");
+                buyerResponse = Console.ReadLine();
                 if (buyerResponse == "y")
                 {
                     Console.WriteLine("The basement is 1000 ft and runs the entire length of the house.");
                     Console.WriteLine("This would be an awesome space for a man cave/ Diva's Destination.");
                     Console.WriteLine();
-                    Console.WriteLine("Would you like to go upstairs?");
-
-                    buyerResponse = Console.ReadLine();
                     Console.ReadLine();
-                
                 }
-                if (buyerResponse =="y")
+
+                Console.WriteLine("Would you like to go upstairs?");
+                buyerResponse = Console.ReadLine();
+                if (buyerResponse == "y")
                 {
                     Console.WriteLine("There are 3 Bedrooms on this floor.");
                     Console.WriteLine("Extra large bedrooms and 1 with a spa master suite");
                     Console.WriteLine();
-                    Console.WriteLine("Would you like to see the extra room?");
-
-                    buyerResponse = Console.ReadLine();
                     Console.ReadLine();
                 }
-               if (buyerResponse == "y")
+
+                Console.WriteLine("Would you like to see the extra room?");
+                buyerResponse = Console.ReadLine();
+                if (buyerResponse == "y")
                 {
                     Console.WriteLine("This room is large enough for an office or babyroom.");
                     Console.WriteLine();
-                    Console.WriteLine("Would you like to see the bathroom?");
-
-                    buyerResponse = Console.ReadLine();
-
+                    Console.ReadLine();
                 }
-                if (buyerResponse == "y")
 
+                Console.WriteLine("Would you like to see the bathroom?");
+                buyerResponse = Console.ReadLine();
+                if (buyerResponse == "y")
                 {
                     Console.WriteLine("The bathroom on this floor is a full bath with a sauna and surround sound.");
-                    Console.WriteLine("Would you like to see the attic?");
-                    Console.WriteLine("This is the last room.");
-
-                    buyerResponse = Console.ReadLine();
                     Console.ReadLine();
-                }  
+                }
+
+                Console.WriteLine("Would you like to see the attic?");
+                Console.WriteLine("This is the last room.");
+                buyerResponse = Console.ReadLine();
                 if (buyerResponse == "y")
                 {
                     Console.WriteLine("The attic is well vented, accesaible and a lot of storage space.");
-                    Console.WriteLine("Would you like to discuss the Comps?");
-                    
-                    buyerResponse = Console.ReadLine();
                 }
+
+                Console.WriteLine("Would you like to discuss the Comps?");
+                buyerResponse = Console.ReadLine();
                 if (buyerResponse == "y")
-
-
                 {
                     Console.WriteLine("Let's go to the office and talk.");
-                       
-                 
                 }
-                else 
+                else
                 {
                     Console.WriteLine("Thank you for selecting this home to view! Call if you have any more questions, here's my card.");
                     Console.WriteLine();
                 }
 
-      
-
+                Console.ReadLine();
             }
-            
         }
     }
 }
