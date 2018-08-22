c3b4f94d3d5bf00d47373418c4c533c0a1e2885f
Chip <chip.elmer@gmail.com>
2018-08-22 09:12:41 -0500

    Added UI class to handle interaction with the user

--- OpenHouse/Program.cs
+++ OpenHouse/Program.cs
@@ -6,45 +6,33 @@ namespace OpenHouse
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Welcome to Carson's Realty Open House!");
             Console.WriteLine();
 
-            Console.WriteLine("Interested in taikng a tour of this beautiful home?");
-            string buyerResponse = Console.ReadLine();
-
-            if (buyerResponse == "y")
+            if (UI.UserAnswersYes("Interested in taikng a tour of this beautiful home?"))
             {
                 Console.WriteLine("Let's start with the Day Room, yes!");
                 Console.WriteLine("This room can be converted into a Livingroom, it is little over 355 sqaure feet.");
                 Console.WriteLine("The Opulent Crystal chandeliar is an early, 1920s original.");
                 Console.WriteLine();
-                Console.WriteLine("Would you like to see the Openfloor plan of the Diningroom and Kitchen?");
-
-                buyerResponse = Console.ReadLine();
-                Console.ReadLine();
-
-                if (buyerResponse == "y")
+                
+                if (UI.UserAnswersYes("Would you like to see the Openfloor plan of the Diningroom and Kitchen?"))
                 {
                     Console.WriteLine("Dinningroom has Wainwritght walls with cheery wood borders enough space tp set 12 comfortably.");
                     Console.WriteLine("Kitchen has stainless steel appliances an gourmet island with granite countertops and authentic Panamian tile shipped from Central America.");
                     Console.WriteLine();
                     Console.ReadLine();
                 }
-
-                Console.WriteLine("Would you like to tour the next see the basement?");
-                buyerResponse = Console.ReadLine();
-                if (buyerResponse == "y")
+                
+                if (UI.UserAnswersYes("Would you like to tour the next see the basement?"))
                 {
                     Console.WriteLine("The basement is 1000 ft and runs the entire length of the house.");
                     Console.WriteLine("This would be an awesome space for a man cave/ Diva's Destination.");
                     Console.WriteLine();
                     Console.ReadLine();
                 }
-
-                Console.WriteLine("Would you like to go upstairs?");
-                buyerResponse = Console.ReadLine();
-                if (buyerResponse == "y")
+                
+                if (UI.UserAnswersYes("Would you like to go upstairs?"))
                 {
                     Console.WriteLine("There are 3 Bedrooms on this floor.");
                     Console.WriteLine("Extra large bedrooms and 1 with a spa master suite");
@@ -52,34 +40,25 @@ namespace OpenHouse
                     Console.ReadLine();
                 }
 
-                Console.WriteLine("Would you like to see the extra room?");
-                buyerResponse = Console.ReadLine();
-                if (buyerResponse == "y")
+                if (UI.UserAnswersYes("Would you like to see the extra room?"))
                 {
                     Console.WriteLine("This room is large enough for an office or babyroom.");
                     Console.WriteLine();
                     Console.ReadLine();
                 }
 
-                Console.WriteLine("Would you like to see the bathroom?");
-                buyerResponse = Console.ReadLine();
-                if (buyerResponse == "y")
+                if (UI.UserAnswersYes("Would you like to see the bathroom?"))
                 {
                     Console.WriteLine("The bathroom on this floor is a full bath with a sauna and surround sound.");
                     Console.ReadLine();
                 }
 
-                Console.WriteLine("Would you like to see the attic?");
-                Console.WriteLine("This is the last room.");
-                buyerResponse = Console.ReadLine();
-                if (buyerResponse == "y")
+                if (UI.UserAnswersYes("Would you like to see the attic?\nThis is the last room."))
                 {
                     Console.WriteLine("The attic is well vented, accesaible and a lot of storage space.");
                 }
 
-                Console.WriteLine("Would you like to discuss the Comps?");
-                buyerResponse = Console.ReadLine();
-                if (buyerResponse == "y")
+                if (UI.UserAnswersYes("Would you like to discuss the Comps?"))
                 {
                     Console.WriteLine("Let's go to the office and talk.");
                 }
---
+++ OpenHouse/UI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenHouse
+{
+    class UI
+    {
+        public static bool UserAnswersYes(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine().ToUpper() == "Y";
+        }
+    }
+}
